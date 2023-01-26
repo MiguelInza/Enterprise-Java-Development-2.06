b701b97bd0e279c4a768748d7308ad7bb60213f2
MiguelInza <inza.miguel@gmail.com>
2023-01-26 19:38:55 +0100

    last commit

--- src/main/java/com/miguel/main/Main.java
+++ src/main/java/com/miguel/main/Main.java
@@ -37,15 +37,30 @@ public class Main {
         Student student3 = new Student("Antonio", 19);
         Student student4 = new Student("Alberto", 25);
 
-        Map<String, Student> studentMap = new HashMap<>();
-        studentMap.put(student1.getName(), student1);
-        studentMap.put(student2.getName(), student2);
-        studentMap.put(student3.getName(), student3);
-        studentMap.put(student4.getName(), student4);
-        System.out.println(studentMap);
-        System.out.println(studentMap.get("Victor"));
+        Map<String, Student> students = new HashMap<>();
+        students.put(student1.getName(), student1);
+        students.put(student2.getName(), student2);
+        students.put(student3.getName(), student3);
+        students.put(student4.getName(), student4);
+        System.out.println(students);
+        System.out.println(students.get("Victor"));
+
+        students = increaseGrades(students);
+
+        System.out.println("Grades after 10% increase: ");
+        for (Map.Entry<String, Student> entry : students.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().getGrade());
+        }
     }
+
+    public static Map<String, Student> increaseGrades(Map<String, Student> students) {
+        for (Map.Entry<String, Student> entry : students.entrySet()) {
+            int newGrade = (int) (entry.getValue().getGrade() * 1.1);
+            entry.getValue().setGrade(newGrade);
+        }
+        return students;
     }
+}
 
 
 
